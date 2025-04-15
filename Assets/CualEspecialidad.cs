396e6f08d22f0125d3fbb2a43d18c5e2940c7cb4
tincho20087 <48802391@est.ort.edu.ar>
2025-04-15 08:57:41 -0300

    Create CualEspecialidad.cs

---
+++ Assets/CualEspecialidad.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CualEspecialidad : MonoBehaviour
+{
+    public int anio;
+    public string nombre;
+    public string especialidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+        if (anio < 3 || anio > 5)
+        {
+            Debug.Log("Año no valido");
+            return;
+        }
+              
+        else if (string.IsNullOrEmpty(nombre))
+        {
+            Debug.Log("Escribe algo en nombre");
+            return;
+        }
+
+        else if (especialidad == "G" || especialidad == "T" || especialidad == "H" || especialidad == "M" || especialidad == "D")
+        {
+            Debug.Log("Muchas gracias " + nombre);
+        }
+
+        else
+        {
+            Debug.Log("Solo puede ingresar T, D, G, M o H");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
